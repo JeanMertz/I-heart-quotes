a53c2deb08e1c152e73708ed6f000153a44f840b
Jean Mertz <jean.mertz@nulvierdrie.nl>
2012-07-27 20:58:08 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IHeartQuotes::Application.config.secret_token = 'ae2d2dea7e352d6d9809e577987f40a6fcae8d89057492c0a04c4314d828a8d06c50d082594686ad8c287c09037299d01cada9d2f22ba94103f2bed5bc588ca0'
