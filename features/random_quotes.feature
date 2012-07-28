2aa58075623c88446619294264fb4ec17e84c162
Jean Mertz <jean@mertz.fm>
2012-07-28 09:29:10 +0200

    Add tests to verify random quote page integrity

--- features/random_quotes.feature
+++ features/random_quotes.feature
@@ -1,9 +1,25 @@
 Feature: Random Quotes
   In order to fake my knowledge of the world to my Twitter followers
   As a visitor of the I <3 Kabisa service
-  I want to be able to view a (random) quote
+  I want to be able to view a random quote
 
   Scenario: Visit quote page
     Given I know my way around the interwebs
     When I visit the I <3 Kabisa website
     Then I should be greeted by a quote
+
+  Scenario: Click permalink
+    Given I am on the random quotes page
+    When I click the permalink
+    Then I should be redirected
+
+  Scenario: Click sourcelink if visible
+    Given I am on the random quotes page
+    And a sourcelink is visible
+    When I click the sourcelink
+    Then I should be redirected
+
+  Scenario: Check quote randomness
+    Given I am on the random quotes page
+    When I reload the page "10" times
+    Then I should see at least two different quotes
--- features/step_definitions/random_quotes_steps.rb
+++ features/step_definitions/random_quotes_steps.rb
@@ -1,12 +1,44 @@
+# encoding: UTF-8
+
 Given /^I know my way around the interwebs$/ do
   # let's just make this an assumption
 end
 
 When /^I visit the I <3 Kabisa website$/ do
   visit '/'
-  current_path.should == '/'
+  current_path.should eq('/')
 end
 
 Then /^I should be greeted by a quote$/ do
-  page.should have_content('this is a static quote')
+  page.should have_xpath("//div[@id='quote_field']", text: /^“.*”$/)
+end
+
+Given /^I am on the random quotes page$/ do
+  visit '/'
+  current_path.should eq('/')
+end
+
+When /^I click the (permalink|sourcelink)$/ do |link|
+  @link_url = find("a.#{link}")['href']
+  click_link(link)
+end
+
+Then /^I should be redirected$/ do
+  current_url.should include(@link_url)
+end
+
+Given /^a sourcelink is visible$/ do
+  page.should have_css('a.sourcelink')
+end
+
+When /^I reload the page "(.*?)" times$/ do |reload|
+  @quotes = []
+  reload.to_i.times do |i|
+    visit '/'
+    @quotes[i] = find('div#quote_field').native.inner_text
+  end
+end
+
+Then /^I should see at least two different quotes$/ do
+  @quotes.uniq.size.should be > 2
 end
