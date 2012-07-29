19863ad248b906c24a5a0d76df44dea07342c8bb
Jean Mertz <jean@mertz.fm>
2012-07-29 08:47:07 +0200

    Add cache_weight method to set cached/external loading ratio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # Set the weight of the cached quotes. This number represents `i` in the
+  # following formula to determine if a cached quote should be shown:
+  #
+  #    rand(100).between?(100-i, 100)
+  #
+  # Setting a higher value significantly recudes page load times because the
+  # service can poll the local database instead of an external API.
+  #
+  # A higher number will slow down the growth of the database as less new
+  # quotes are gathered and stored locally.
+  #
+  # Recommended values:
+  #  | cached quotes | cache_weight |
+  #  +---------------+--------------+
+  #  | 0 - 100       | 1            | rapid database growth
+  #  | 101 - 500     | 25           |
+  #  | 501 - 1000    | 50           |
+  #  | 1000+         | 90           | almost no new records saved
+  #
+  def cache_weight
+    25
+  end
 end
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,18 @@
 class QuotesController < ApplicationController
+
   def random
+    if show_cached_quote?
+      # show cached quote
+    else
+      # show external quote
+    end
+
+    render :show
+  end
+
+  # Weight based calculation to determine frequency of cached quotes.
+  #
+  def show_cached_quote?
+    rand(100).between?(100 - cache_weight, 100)
   end
 end
